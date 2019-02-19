d1ce957f5d27f1543e9c794ac0aa02a50cf55d7d
Richard Bates <richard.bates@babylonhealth.com>
2019-02-19 22:45:06 +0000

    Dockerise app

--- template.rb
+++ template.rb
@@ -178,3 +178,45 @@ USER
 git :init
 git add: '.'
 git commit: "-m 'Initial app setup'"
+
+# Dockerise app
+create_file 'Dockerfile', <<~DOCKERFILE
+  # Base image
+  FROM ruby:2.3.6
+
+  # Install dependencies
+  RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+
+  # Create and set working directory
+  RUN mkdir /app
+  WORKDIR /app
+
+  # Copy gemfile and install gems
+  COPY Gemfile /app/Gemfile
+  COPY Gemfile.lock /app/Gemfile.lock
+  RUN bundle install
+
+  # Copy application code
+  COPY . /app
+
+  # Run the app
+  CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
+DOCKERFILE
+
+create_file 'docker-compose.yml', <<~COMPOSE
+  version: '3'
+  services:
+  db:
+    image: postgres
+    volumes:
+      - ./tmp/db:/var/lib/postgresql/data
+  web:
+    build: .
+    command: bash -c "bundle exec rails s -p 3000 -b '0.0.0.0'"
+    volumes:
+      - .:/myapp
+    ports:
+      - "3000:3000"
+    depends_on:
+      - db
+COMPOSE
