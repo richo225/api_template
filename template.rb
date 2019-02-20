077e6f22ed2ce55c1c6f4626e82b3b0d34c393a0
Richard Bates <richard.bates@babylonhealth.com>
2019-02-20 19:29:19 +0000

    Creates makefile

--- template.rb
+++ template.rb
@@ -220,3 +220,23 @@ create_file 'docker-compose.yml', <<~COMPOSE
     depends_on:
       - db
 COMPOSE
+
+create_file 'Makefile', <<~MAKE
+  default: build
+
+  build: config
+      docker-compose build
+
+  start: setup
+    docker-compose up
+
+  db-setup:
+    docker-compose run web bundle exec rake db:setup --trace
+
+  compose-run:
+    docker-compose up --build -d
+    sleep 20
+
+  tests:
+    docker-compose run web bundle exec rake spec
+MAKE
